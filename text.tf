26da8f3f07798bef050583156ca14b16d6a37bd8
Amangithub95 <139551938+Amangithub95@users.noreply.github.com>
2023-08-22 19:30:45 +0530

    Add files via upload

---
+++ text.tf
@@ -0,0 +1,4 @@
+resource "local_file" "wild" {
+  filename = "var.filename"
+  content  = "var.content"
+}
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "filename" { 
+default = "lion.txt"
+}
+variable "content" { 
+default = "King of jungle"
+}
