02d69e1a091d63cfb3c6a3ca9b3b591d0f29b6aa
Amangithub95 <139551938+Amangithub95@users.noreply.github.com>
2023-08-24 15:35:24 +0530

    Update and rename day3.tf to day.tf

---
+++ day.tf
@@ -0,0 +1,10 @@
+resource "local_file" "my-cat" {
+  filename = var.filename
+  content = var.content
+}
+variable "filename" { 
+default = "cat.txt"
+}
+variable "content" { 
+default = "i love cats"
+}
